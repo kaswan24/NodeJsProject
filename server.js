64e7dd9b6bed37b3c72af393dc32e3ee4371e736
kaswan24 <ankitkaswan04@gmail.com>
2020-04-17 04:23:29 +0530

    updated server static path again

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-// app.use('/',express.static('public')); 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('public'));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-    });
-  }
+app.use('/',express.static('public')); 
 app.use('/api', require('./Routes/api').route)
 
 app.listen(PORT, () => console.log('server running at port 5000'))
