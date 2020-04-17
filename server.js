e715703219a88ae2894826db3524fb075b6961ec
kaswan24 <ankitkaswan04@gmail.com>
2020-04-17 08:56:07 +0530

    path didnt worked changed it a little again

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-// app.use(express.static(path.join(__dirname,'public'))); 
-app.use('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.use(express.static(path.join(__dirname,'public'))); 
+
 app.use('/api', require('./Routes/api').route)
 
 app.listen(PORT, () => console.log('server running at port 5000'))
