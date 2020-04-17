02397b1a5bb27b107668fa2e2be42520f299716a
kaswan24 <ankitkaswan04@gmail.com>
2020-04-17 08:51:29 +0530

    changed app.use to app.get for static page

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/',express.static(path.join(__dirname,'public'))); 
+// app.use(express.static(path.join(__dirname,'public'))); 
+app.use('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 app.use('/api', require('./Routes/api').route)
 
 app.listen(PORT, () => console.log('server running at port 5000'))
